c005241730ab693b1da8ca3733bbbb661d10a764
Olagoke Emmanuel <olagokeemmanuel27@gmail.com>
2024-04-03 20:55:50 +0100

    base apparel

---
+++ vite-project-app/src/App.jsx
@@ -0,0 +1,17 @@
+/* import { useState } from 'react' */
+import './App.css'
+import Hero from './Component/Hero'
+
+
+function App() {
+  
+  return (
+    <div>
+      <Hero/>
+    </div>
+  )
+
+
+}
+
+export default App
